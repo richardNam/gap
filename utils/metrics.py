e1d1f4ce9996625e3de14b6b9b7fe2b9fad70e82
Richard <richardhnam@gmail.com>
2019-02-21 00:01:59 -0800

    add metrics outline

---
+++ utils/metrics.py
@@ -0,0 +1,24 @@
+
+
+__all__ = [
+        'precision_score',
+        'recall_score',
+        'f1_score',
+        'fbeta_score',
+    ]
+
+class Score(object):
+    def __init__(self):
+
+    def precision_score(self):
+        pass
+
+    def recall_score(self):
+        pass
+
+    def f1_score(self): 
+        pass
+
+    def fbeta_score(self):
+        pass
+
--- utils/types.py
+++ utils/types.py
@@ -1,7 +1,8 @@
 from collections import namedtuple
 
 __all__ = [
-        'Sample'
+        'Sample',
+        'PredictedClass',
         ]
 
 
@@ -20,5 +21,45 @@ class Sample(namedtuple(
         'b_coref', 
         'url'
     ])):
+    """ A wrapper for the raw input data (training and validation set). The
+    data source is located at: 
+        https://github.com/google-research-datasets/gap-coreference
+
+    Args:
+        id (str):
+        text (str):
+        pronoun (str):
+        pronoun_offset (int):
+        a (str):
+        a_offset (int):
+        a_coref (bool):
+        b (str):
+        b_offset (int):
+        b_coref (bool):
+        url (str):
+
+    """
+    pass
+
+class PredictedClass(nampedtuple(
+    'PredictedClass',
+    [
+        'index',
+        'label',
+        'predicted_label',
+        'outcome',
+    ])):
+    """ A tuple for evaluating model performance. Wraps a model predicition with
+    its true label and other meta data.
+
+    Args:
+        index (int): The index of the text from which the prediction is
+            associated with.
+        label (int): The ground truth class of the input text.
+        predicted_label (int): The predicted class from the model. The 
+            class should be a hard assignment.
+        outcome (str): Acronym for the evaluated prediction ie. TP, TN, FP, FN.
+
+    """
     pass
 
